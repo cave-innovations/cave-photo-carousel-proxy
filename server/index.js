0b26c3ecb1ab834571dc68c5b970c342697418d8
Michael Lee <michaellee@Michaels-MacBook-Pro-2.local>
2019-09-10 10:14:52 -0700

    deployed final

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 4000;
 
+app.use('/listing/:id', express.static('public'));
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
